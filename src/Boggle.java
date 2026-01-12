ad8546d89492dfb7722c18e0c64c7ef6b96a55b4
L-Tran <logan.tran@menloschool.org>
2026-01-11 21:07:04 -0800

    Final commit

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Stack;
 
 // Completed by Logan Tran
 public class Boggle {
@@ -15,6 +13,7 @@ public class Boggle {
         ArrayList<String> goodWords = new ArrayList<String>();
         boardSize = board.length;
 
+        // Initialize array to indicate visited squares
         isVisited = new boolean[boardSize][boardSize];
 
         // Create TST for dictionary
@@ -31,27 +30,17 @@ public class Boggle {
 
             }
         }
-        // Convert the list into a sorted array of strings, then return the array.
-//        String[] sol = new String[goodWords.size()];
-//        goodWords.toArray(sol);
-//        Arrays.sort(sol);
-//        return sol;
 
-        HashSet<String> sol = new HashSet<String>();
-        for(String s: goodWords) {
-            sol.add(s);
-        }
-        String[] noDup = new String[sol.size()];
-        int i = 0;
-        for(String s: sol) {
-            noDup[i] = s;
-            i++;
-        }
-        Arrays.sort(noDup);
-        return noDup;
+        // Convert the list into a sorted array of strings, then return the array.
+        String[] sol = new String[goodWords.size()];
+        goodWords.toArray(sol);
+        Arrays.sort(sol);
+        return sol;
     }
 
     public static void dfs(int row, int col, String word, char[][] board, ArrayList<String> goodWords) {
+
+        // Base cases for: outside squares and if square is visited
         if (row < 0 || col < 0 || row >= boardSize || col >= boardSize) {
             return;
         }
@@ -59,24 +48,31 @@ public class Boggle {
             return;
         }
 
+        // Add next square letter
         word += board[row][col];
 
-        // If the word is not a valid prefix
+        // If the new word is not a valid prefix
         if(dict.search(word) == null) {
             return;
         }
 
+        // Check if prefix is a word
         if(dict.search(word).isWord()) {
+            // Add to words and set word in TST to false in order to prevent duplicates
             goodWords.add(word);
+            dict.search(word).setWord(false);
         }
 
+        // Set visited to true when going through square
         isVisited[row][col] = true;
 
+        // DFS all possible directions
         dfs(row + 1, col, word, board, goodWords);
         dfs(row, col + 1, word, board, goodWords);
         dfs(row - 1, col, word, board, goodWords);
         dfs(row, col - 1, word, board, goodWords);
 
+        // Set visited to false after tracing path down to prepare for next
         isVisited[row][col] = false;
     }
 }
